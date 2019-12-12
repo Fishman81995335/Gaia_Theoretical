8c24f05c0e38e02e288bb2f0397f963e567960de
Vineet Kamat <vineetkamat99@gmail.com>
2019-12-12 15:12:37 -0500

    various additions and fixing of error

--- err_flux_combined.py
+++ err_flux_combined.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.patches as mpatches
 import random as r
 import statistics as stats
+
 #
 #
 # Takes in folder directory of randomly sampled gaia dr2 sources as csv files
@@ -27,21 +28,20 @@ mic_amt = 2
 c = 25.68836
 
 
-
 def get_err(flux):
-    mag = (c - 2.5*np.log10(flux))
-    z = max([10.**((0.4)*(12 - 15)), 10.**((0.4)*(mag - 15))])
-    mag_theo_err = (.001*(((0.04895 * z**2) + (1.8633 * z) + 0.0001985)**(1/2)))
+    mag = c - 2.5 * np.log10(flux)
+    z = max([10.0 ** ((0.4) * (12 - 15)), 10.0 ** ((0.4) * (mag - 15))])
+    mag_theo_err = 0.001 * (((0.04895 * z ** 2) + (1.8633 * z) + 0.0001985) ** (1 / 2))
     val_1 = mag + mag_theo_err
     val_2 = mag - mag_theo_err
-    f_v_1 = 10**((c-val_1)/2.5)
-    f_v_2 = 10**((c-val_2)/2.5)
-    return (f_v_2-f_v_1)/2
+    f_v_1 = 10 ** ((c - val_1) / 2.5)
+    f_v_2 = 10 ** ((c - val_2) / 2.5)
+    return (f_v_2 - f_v_1) / 2
+
 
 for file_name in os.listdir(file_dir):
     err_vals = []
     flux_vals = []
-
 
     flux_vals_2 = []
     mag_vals = []
@@ -49,7 +49,6 @@ for file_name in os.listdir(file_dir):
     nums = []
     micro_bool = []
     datum = []
-
 
     print("processing file " + str(file_name) + " \U0001f44d")
     line_num = 0
@@ -63,63 +62,62 @@ for file_name in os.listdir(file_dir):
             err_vals.append(float(line[3]))
             flux_vals.append(float(line[2]))
 
-
             mic_or_not = r.random()
-            if mic_or_not <.05:
+            if mic_or_not < 0.05:
                 micro_bool.append(True)
             else:
                 micro_bool.append(False)
             nums.append(int(line[1]))
-            datum.append((float(line[2]),int(line[1])))
+            datum.append((float(line[2]), int(line[1])))
 
     print("processing graph")
     plt.xlabel("log flux")
     plt.ylabel("log err")
     plt.title("Flux vs Error")
-    blue = 'No microlensing'
-    red = 'Microlensed 1 event by factor of '+ str(mic_amt)
-    green = 'Actual data'
-    blue_patch = mpatches.Patch(color='blue', label=blue)
-    red_patch = mpatches.Patch(color='red', label=red)
-    green_patch = mpatches.Patch(color='green', label=green)
-    plt.legend(bbox_transform=plt.gcf().transFigure, bbox_to_anchor = (1,1),handles=[blue_patch, red_patch, green_patch])
-    plt.scatter(np.log(flux_vals),np.log(err_vals),c='g',s=2)
+    blue = "No microlensing"
+    red = "Microlensed 1 event by factor of " + str(mic_amt)
+    green = "Actual data"
+    blue_patch = mpatches.Patch(color="blue", label=blue)
+    red_patch = mpatches.Patch(color="red", label=red)
+    green_patch = mpatches.Patch(color="green", label=green)
+    plt.legend(
+        bbox_transform=plt.gcf().transFigure,
+        bbox_to_anchor=(1, 1),
+        handles=[blue_patch, red_patch, green_patch],
+    )
+    plt.scatter(np.log(flux_vals), np.log(err_vals), c="g", s=2)
 
     # create cdf for fluxes
-    datum.sort(key=lambda tup:tup[1])
+    datum.sort(key=lambda tup: tup[1])
 
-
-    length = len(flux_vals)-1
+    length = len(flux_vals) - 1
     stdev = []
     mean = []
     col = []
     for n in range(1000):
-            if micro_bool[n]:
-data = datum[r.randint(0,length)]
-flux = data[0]
-err = get_err(flux)
-num = data[1]
-s = np.random.normal(abs(flux),abs(err),abs(num))
-to_change = r.randint(0,num-1)
-s[to_change] = s[to_change]*mic_amt
-stdev.append(stats.stdev(s))
-mean.append(stats.mean(s))
-                col.append('r')
-            else:
-                data = datum[r.randint(0,length)]
-                flux = data[0]
-                err = get_err(flux)
-                num = data[1]
-                s = np.random.normal(abs(flux),abs(err),abs(num))
-                stdev.append(stats.stdev(s))
-                mean.append(stats.mean(s))
-                col.append('b')
-
-
-
+        if micro_bool[n]:
+            data = datum[r.randint(0, length)]
+            flux = data[0]
+            err = get_err(flux)
+            num = data[1]
+            s = np.random.normal(abs(flux), abs(err), abs(num))
+            to_change = r.randint(0, num - 1)
+            s[to_change] = s[to_change] * mic_amt
+            stdev.append(stats.stdev(s) / np.sqrt(num))
+            mean.append(stats.mean(s))
+            col.append("r")
+        else:
+            data = datum[r.randint(0, length)]
+            flux = data[0]
+            err = get_err(flux)
+            num = data[1]
+            s = np.random.normal(abs(flux), abs(err), abs(num))
+            stdev.append(stats.stdev(s) / np.sqrt(num))
+            mean.append(stats.mean(s))
+            col.append("b")
 
     print("processing graph")
-    plt.scatter(np.log(mean),np.log(stdev),c=col,s=2)
-    plt.savefig(plots_dir + "/" + file_name + ".png")
+    plt.scatter(np.log(mean), np.log(stdev), c=col, s=2)
+    plt.savefig(plots_dir + "/" + file_name + ".png", dpi=300)
     plt.close()
 
---
+++ sigma_ratio_ml_nml.py
@@ -0,0 +1,114 @@
+import csv
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import matplotlib.patches as mpatches
+import random as r
+import statistics as stats
+
+#
+#
+# Takes in folder directory of randomly sampled gaia dr2 sources as csv files
+# plots err and flux on x y axis
+#
+#
+file_dir = "/Users/vineetkamat/Documents/College/Fifth Semester/Research/Gaia_Theoretical/smaller test samples"
+
+
+plots_dir = "/Users/vineetkamat/Documents/College/Fifth Semester/Research/ml_nml_2"
+
+
+count = 0
+
+mag_min = 20
+mag_max = 12
+num_min = 5
+num_max = 50
+mic_amt = 2
+c = 25.68836
+
+
+def get_err(flux):
+    mag = c - 2.5 * np.log10(flux)
+    z = max([10.0 ** ((0.4) * (12 - 15)), 10.0 ** ((0.4) * (mag - 15))])
+    mag_theo_err = 0.001 * (((0.04895 * z ** 2) + (1.8633 * z) + 0.0001985) ** (1 / 2))
+    val_1 = mag + mag_theo_err
+    val_2 = mag - mag_theo_err
+    f_v_1 = 10 ** ((c - val_1) / 2.5)
+    f_v_2 = 10 ** ((c - val_2) / 2.5)
+    return (f_v_2 - f_v_1) / 2
+
+
+for file_name in os.listdir(file_dir):
+    err_vals = []
+    flux_vals = []
+
+    mag_vals = []
+    nums = []
+    datum = []
+    sources = []
+
+    print("processing file " + str(file_name) + " \U0001f44d")
+    line_num = 0
+    file_path = file_dir + "/" + file_name
+    with open(file_path, "r") as fil:
+        fil_reader = csv.reader(fil)
+        for line in fil_reader:
+            if line_num == 0:
+                line_num = line_num + 1
+                continue
+            err_vals.append(float(line[3]) * (np.sqrt(float(line[1]))))
+            flux_vals.append(float(line[2]))
+            nums.append(int(line[1]))
+            datum.append((float(line[2]), int(line[1])))
+            sources.append(int(line[0]))
+
+    print("processing graph")
+
+    # create cdf for fluxes
+    datum.sort(key=lambda tup: tup[1])
+
+    length = len(flux_vals) - 1
+    stdev_rat = []
+    means = []
+    col = []
+    nums = []
+    for n in range(len(datum)):
+        data = datum[n]
+        flux = data[0]
+        theo_err = get_err(flux)
+        num = data[1]
+        s = np.random.normal(abs(flux), abs(theo_err), abs(num))
+        rat = stats.stdev(s) / err_vals[n]
+        stdev_rat.append(rat)
+        if rat > 100:
+            print(sources[n])
+        means.append(stats.mean(s))
+        nums.append(num)
+
+    print("processing graph")
+    plt.xlabel("log flux")
+    plt.ylabel("log ml/nml theoretical")
+    plt.title("Log Flux vs Error Rat")
+    green = "Ratio of theoretical microlensed sigma to real sigma" + str(mic_amt)
+    green_patch = mpatches.Patch(color="green", label=green)
+    plt.legend(
+        bbox_transform=plt.gcf().transFigure,
+        bbox_to_anchor=(1, 1),
+        handles=[green_patch],
+    )
+    plt.scatter(np.log(means), np.log(stdev_rat), c="g", s=2)
+    plt.savefig(plots_dir + "/" + file_name + " by mean" ".png", dpi=300)
+    plt.close()
+
+    plt.xlabel("num obs")
+    plt.ylabel("log ml/nml theoretical")
+    plt.title("Num Obs vs Error Rat")
+    red = "Ratio of theoretical microlensed sigma to real sigma" + str(mic_amt)
+    red_patch = mpatches.Patch(color="red", label=red)
+    plt.legend(
+        bbox_transform=plt.gcf().transFigure, bbox_to_anchor=(1, 1), handles=[red_patch]
+    )
+    plt.scatter(nums, np.log(stdev_rat), c="r", s=2)
+    plt.savefig(plots_dir + "/" + file_name + " by num" ".png", dpi=300)
+    plt.close()
